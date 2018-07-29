f057577a923f8b76bbe3789097dbf40e89dff390
Renz Kenneth Busarang <renzbusarang>
2018-07-29 11:17:57 +0800

    Change sass to scss creating the admin part

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReceptionistComponent } from '../assets/ts/receptionist.component';
-import { AdminComponent } from '../assets/ts/admin.component';
+import { AdminComponent } from '../assets/Admin/ts/admin.component';
 import { OccupantComponent } from '../assets/ts/occupant.component';
 import { ReceptionistDineComponent } from '../assets/ts/receptionist-dine.component';
 import { ReceptionistAssistanceComponent } from '../assets/ts/receptionist-assistance.component';
 import { ReceptionistOrderComponent } from '../assets/ts/receptionist-order.component';
 import { OccupantDishDetailsComponent } from '../assets/ts/occupant-dish-details.component';
 import { OccupantMenuComponent } from '../assets/ts/occupant-menu.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+//Admin 
+
 
 @NgModule({
   declarations: [
@@ -22,11 +26,13 @@ import { OccupantMenuComponent } from '../assets/ts/occupant-menu.component';
     ReceptionistAssistanceComponent,
     ReceptionistOrderComponent,
     OccupantDishDetailsComponent,
-    OccupantMenuComponent
+    OccupantMenuComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-app/src/assets/Admin/ts/admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: '../html/admin.component.html',
+  styleUrls: ['../sass/style.scss']
+})
+export class AdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
