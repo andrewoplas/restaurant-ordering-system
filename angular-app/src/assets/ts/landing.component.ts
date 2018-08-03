e743c863411ae159bf22fab504a1ee029e6f8362
Timothy Ouano <timothyouano@gmail.com>
2018-08-03 15:42:14 +0800

    Frontend landing with mobile responsiveness

--- angular-app/src/assets/ts/landing.component.ts
+++ angular-app/src/assets/ts/landing.component.ts
@@ -35,9 +35,62 @@ export class LandingComponent implements OnInit {
     // Animate rotation big circle container
     AnimateRotate(".circle-container-big", 360);
 
+
+    // Distance Calculation
+    var windowWidth = $(window).width();
+    var marg = "0 0 0 0";
+    var margSM = "32vw 0 0 21vw";
+
+    if(windowWidth > 1072){
+      marg = "-55vw 0 0 -20vw";
+    }
+    else if(windowWidth <= 1072 && windowWidth > 925){
+      marg = "-50vw 0 0 -20vw";
+    }
+    else if(windowWidth <= 925 && windowWidth > 825){
+      marg = "-40vw 0 0 -20vw";
+      $("h1").css("font-size", "92px");
+      $("a").css("font-size", "42px");
+    }
+    else if(windowWidth <= 825 && windowWidth > 600){
+      marg = "-65vw 0 0 -20vw";
+      $(".circle-container-sm").css("box-shadow", "none");
+      $(".featured-image").css("width", "75vh");
+      $(".featured-image").css("height", "60vh");
+      $(".circle-container-big").css("display", "flex");
+      $(".circle-container-big").css("justify-content", "center");
+      $(".right-container").css("margin", "40vh auto 0 auto");
+      $(".right-container").css("right", "auto");
+      $(".right-container").css("width", "auto");
+      $("h1").css("font-size", "55px");
+      $("a").css("font-size", "28px");
+    }
+    else if(windowWidth <= 600) {
+      marg = "-40vw 0 0 -20vw";
+      $(".circle-container-sm").css("box-shadow", "none");
+      $(".circle-container-big").css("display", "flex");
+      $(".circle-container-big").css("justify-content", "center");
+      $(".right-container").css("margin", "40vh auto 0 auto");
+      $(".right-container").css("right", "auto");
+      $(".right-container").css("width", "auto");
+      $("h1").css("font-size", "55px");
+      $("a").css("font-size", "28px");
+
+      margSM = "32vw 0 0 0";
+      $(".featured-image").css("width", "100%");
+      $(".featured-image").css("height", "100%");
+      $(".featured-image").css("right", "0px");
+      $(".featured-image").css("margin-bottom", "-40px");
+    }
+
+    // Featured Image distance from right
+    if(windowWidth <= 925 && windowWidth > 600){
+      $(".featured-image").css("right", "8vw");
+    }
+
     // Animate big circle container placement
     $(".circle-container-big").animate({
-        margin: "-120vh 0 0 -20vw"
+        margin: marg
     }, 1000, function() {
 
       // Animate rotation small circle container
@@ -45,18 +98,61 @@ export class LandingComponent implements OnInit {
 
       // Animate small circle container placement
       $(".circle-container-sm").animate({
-          margin: "70vh 0 0 21vw"
+          margin: margSM
       }, 1000, function() {
         
         // Animate Featured Photo fade in
         $(".featured-image").animate({
             opacity: "1"
         }, 1000, function() {
-          // After animation
+          
+          // Show Call to Action
+          $(".right-call-to-action").animate({
+              opacity: "1"
+          }, 1000, function() {
+            // After Animation
+          });
         });
       });
     });
 
+    // Media Query JS
+
+    $(window).resize(function(){
+
+      if($(window).width() > 1072){
+        $(".circle-container-big").css("margin-top", "-55vw");
+      }
+      if ($(window).width() <= 1072 && $(window).width() > 925) {  
+        $(".circle-container-big").css("margin-top", "-50vw");
+      }
+      else if ($(window).width() <= 925 && $(window).width() > 825) {  
+        $(".circle-container-big").css("margin-top", "-40vw");
+        $(".featured-image").css("right", "8vw")
+      }
+      else if($(window).width() <= 825 && $(window).width() > 600){
+        $(".circle-container-big").css("margin-top", "-65vw");
+        $(".circle-container-sm").css("box-shadow", "none");
+        $(".circle-container-sm").css("margin", "32vw 0 0 21vw");
+        $(".featured-image").css("width", "75vh");
+        $(".featured-image").css("height", "60vh");
+        $(".circle-container-big").css("display", "flex");
+        $(".circle-container-big").css("justify-content", "center");
+        $(".right-container").css("margin", "40vh auto 0 auto");
+        $(".right-container").css("right", "auto");
+        $("h1").css("font-size", "55px");
+        $("a").css("font-size", "28px");
+        $(".right-container").css("width", "auto");
+      }
+      else if($(window).width() <= 600) {
+        $(".circle-container-big").css("margin-top", "-40vw");
+        $(".circle-container-sm").css("margin-left", "0");
+        $(".featured-image").css("width", "100%");
+        $(".featured-image").css("height", "100%");
+        $(".featured-image").css("right", "auto");
+      }
+
+    });
     
 
   }
