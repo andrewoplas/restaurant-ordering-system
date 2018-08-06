df48c1f472619b6f78a82dae35623a84a657e211
Andrew Oplas <andrewmoplas@gmail.com>
2018-08-06 20:55:39 +0800

    Finished Task/#0019

--- angular-app/src/assets/ts/occupant-menu.component.ts
+++ angular-app/src/assets/ts/occupant-menu.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from "jquery";
+
 @Component({
   selector: 'app-occupant-menu',
   templateUrl: '../html/occupant-menu.component.html',
   styleUrls: ['../scss/occupant-menu.component.scss']
 })
 export class OccupantMenuComponent implements OnInit {
-
+    
   constructor() { }
 
   ngOnInit() {
