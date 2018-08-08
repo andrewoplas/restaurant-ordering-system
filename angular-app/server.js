ab0302842f8d2917abbab7dfc09767d88b8b52f7
Timothy Ouano <timothyouano@gmail.com>
2018-08-08 17:12:02 +0800

    express application update dist routing

--- angular-app/server.js
+++ angular-app/server.js
@@ -22,7 +22,7 @@ app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/restaurant-ordering-system'));
 
 // Start the app by listening on the default
 // Heroku port
