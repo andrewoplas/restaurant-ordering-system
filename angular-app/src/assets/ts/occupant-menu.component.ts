998e2e93f8c9ec5f1bead5baee0655fecaec44ff
Andrew Oplas <andrewmoplas@gmail.com>
2018-08-11 01:37:58 +0800

    [Task/#0019] - Added animation to menu items

--- angular-app/src/assets/ts/occupant-menu.component.ts
+++ angular-app/src/assets/ts/occupant-menu.component.ts
@@ -16,36 +16,52 @@ export class OccupantMenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    $(document).on('click', '.menu', function(){
-      $('.menu').removeClass('active');
-      $(this).addClass("active");
+    this.initializeMenuItems();
+    this.clickMenu();
+    this.clickFloatingActionButton();
+  }
+
+  initializeMenuItems() {
+    $(document).ready(function(){
+        $(".menu-item")
+          .removeClass("hide")
+          .addClass("animated zoomIn fast");
     });
+  }
 
-    $('.fab-menu').click(function(){
-        let isActive = $('.fab-menu-expand').hasClass('active');
-        
-        if(!isActive) {
-            $(".fab-menu-expand").addClass("active").removeClass("inactive");
-            $(this).find('i').text('close');
+  clickMenu() {
+      $(document).on('click', '.menu', function () {
+          $('.menu').removeClass('active');
+          $(this).addClass("active");
+      });
+  }
 
-            setTimeout(function () { 
-                let menuItems = $('.menu-item-xs');
-                menuItems.removeClass('fadeOut');
+  clickFloatingActionButton() {
+      $('.fab-menu').click(function () {
+          let isActive = $('.fab-menu-expand').hasClass('active');
 
-                for(let i=1; i<=menuItems.length; i++) {
-                    setTimeout(function() {
-                        $(menuItems[i - 1]).removeClass("hide").addClass("fadeIn");
-                    }, i / 2 * 75);
-                }
-             }, 400);
-            
-        } else {
-            $(".menu-container-xs").addClass("hide");
-            $(".menu-item-xs").removeClass("fadeIn").addClass("hide");
-            $(".fab-menu-expand").addClass("inactive").removeClass("active");
-            $(this).find('i').text('restaurant_menu');            
-        }
-    });
+          if (!isActive) {
+              $(".fab-menu-expand").addClass("active").removeClass("inactive");
+              $(this).find('i').text('close');
+
+              setTimeout(function () {
+                  let menuItems = $('.menu-item-xs');
+                  menuItems.removeClass('fadeOut');
+
+                  for (let i = 1; i <= menuItems.length; i++) {
+                      setTimeout(function () {
+                          $(menuItems[i - 1]).removeClass("hide").addClass("fadeIn");
+                      }, i / 2 * 75);
+                  }
+              }, 400);
+
+          } else {
+              $(".menu-container-xs").addClass("hide");
+              $(".menu-item-xs").removeClass("fadeIn").addClass("hide");
+              $(".fab-menu-expand").addClass("inactive").removeClass("active");
+              $(this).find('i').text('restaurant_menu');
+          }
+      });
   }
 
 }
