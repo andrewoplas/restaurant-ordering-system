cece289d0383830ada9ee2fa1ff37011c01711ef
Andrew Oplas <andrewmoplas@gmail.com>
2018-09-23 18:23:30 +0800

    [Task/#0059] - Fixed template of Menu Item Component

--- angular-app/src/assets/Admin/ts/menu-item.component.ts
+++ angular-app/src/assets/Admin/ts/menu-item.component.ts
@@ -14,20 +14,21 @@ export class MenuItemComponent implements OnInit {
 
 
   ngOnInit() {
-    $('#editable-datatable').dataTable({
-      "columnDefs": [ {
-        "targets": [0, 5],
-        "orderable": false
-        } ],
-        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
-        "dom": "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
-        "<'row'<'col-sm-12'tr>>" +
-        "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>"
-    });
     $('input[type=file]').change(function () {
       var filePath = $(this).val().replace(/^.*[\\\/]/, '');
       $("#file-name").text(filePath); 
-  });
+    });
+  }
+
+  ngAfterViewInit() {
+    $("#table-menu-item").dataTable({
+      columnDefs: [
+        {
+          targets: [0, 5],
+          orderable: false
+        }
+      ]
+    });
   }
 
 }
