fab2bc328e57a42f6e7ff679a197922782119c52
Andrew Oplas <andrewmoplas@gmail.com>
2018-09-25 07:53:23 +0800

    [Task/#0066] - Generated Menu model

---
+++ angular-app/src/app/core/entity/Menu.ts
@@ -0,0 +1,13 @@
+export class Order {
+  id: number;
+  name: string;
+  description: string;
+  menu_items: Array<number>;
+
+  constructor(id:number, name:string, description: string, menu_items: Array<number>) {
+    this.id = id
+    this.name = name
+    this.description = description
+    this.menu_items = menu_items
+  }
+}
