42d1ec29b8d8e5fba17132bb02de9c0af3bdc078
Andrew Oplas <andrewmoplas@gmail.com>
2018-09-25 08:06:49 +0800

    [Task/#0066] - Generated Menu component

---
+++ angular-app/src/assets/Admin/ts/menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: '../html/menu.component.html',
+  styleUrls: ['../scss/menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
