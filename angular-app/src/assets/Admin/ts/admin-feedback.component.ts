5d6c27f92be9c1db87b94194af45600e2c9b520f
Timothy Ouano <timothyouano@gmail.com>
2018-10-05 12:28:56 +0800

    [Task/#0077] - Implemented UI for Feedback Admin

--- angular-app/src/assets/Admin/ts/admin-feedback.component.ts
+++ angular-app/src/assets/Admin/ts/admin-feedback.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../../../app/core/services/order.service';
+import { Order, Status } from '../../../app/models/Order';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import * as $ from 'jquery';
+import 'datatables.net';
 
 @Component({
   selector: 'app-admin-feedback',
@@ -6,10 +11,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../scss/admin-feedback.component.scss']
 })
 export class AdminFeedbackComponent implements OnInit {
+  title = 'FEEDBACK';
+  status = Status;
+  orderModal;
+  orderList: Array<Order> = new Array<Order>();
 
-  constructor() { }
+  constructor(private orderService: OrderService, private modalService: NgbModal) {}
 
   ngOnInit() {
+    $("#table-orders").dataTable();
+    $('.preloader').fadeOut();
+  }
+
+  reinitialize(isLast: boolean) {
+    if (isLast && !$.fn.DataTable.isDataTable('#table-orders')) {
+      $('#table-orders').dataTable();
+      eval("$('[data-toggle=tooltip]').tooltip();");
+    }
+  }
+
+  delete(id: number) {
+    this.orderService.deleteOrder(id)
+      .subscribe(
+        success => {
+          if(success) {
+            this.orderList = this.orderList.filter(o => o.id != id);
+            // alert('done');
+          } else {
+            // alert('error');
+          }
+      });
+  }
+
+  open(content: any, order: Order) {
+    this.orderModal = order;
+    this.modalService.open(content, { 
+      ariaLabelledBy: 'modal-basic-title' ,
+      centered: true,
+      windowClass: 'order-items-modal'
+    }).result.then((result) => {
+    }, (reason) => {
+      // close
+    });
   }
 
 }
