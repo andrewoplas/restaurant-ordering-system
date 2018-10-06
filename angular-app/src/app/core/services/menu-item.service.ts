07563375eab88d22fdc9838c2e2d9f6fe14638e8
Patrick <papelias1337@gmail.com>
2018-10-06 11:14:00 +0800

    [Task/#0055] - added validation in add menu form

---
+++ angular-app/src/app/core/services/menu-item.service.ts
@@ -0,0 +1,98 @@
+
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+import { MessageService } from "../services/message.service";
+import { Observable } from "rxjs/Observable";
+import { catchError, tap } from "rxjs/operators";
+import { of } from "rxjs/observable/of";
+import { MenuItem } from '../../models/MenuItem';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+};
+
+@Injectable({
+  providedIn: "root"
+})
+export class MenuItemService {
+  private baseUrl: string;
+  public menuItem: Array<MenuItem>;
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {
+    this.baseUrl = "http://localhost:8080";
+  }
+
+  get menuItemList(): Array<MenuItem> {
+    return this.menuItem;
+  }
+
+  /** GET: retrieve menuItem from the server */
+  public getAllMenuItems(): Observable<MenuItem[]> {
+    return this.http
+      .get<MenuItem[]>(`${this.baseUrl}/get-all-items`, httpOptions)
+      .pipe(
+        tap(() => this.log("get-all-items")),
+        catchError(this.handleError("get-all-items", []))
+      );
+  }
+
+  /** POST: add a new menu to the server */
+  addMenuItem(menuItem: MenuItem): Observable<any> {
+    return this.http
+      .put<MenuItem>(`${this.baseUrl}/add-item`, menuItem, httpOptions)
+      .pipe(
+        tap(_ => this.log(`add item with id=${menuItem.id}`)),
+        catchError(this.handleError<MenuItem>("add-item"))
+      );
+  }
+
+  /** DELETE: delete the menu from the server */
+  public deleteMenuItem(menuItem: string | number): Observable<any> {
+    return this.http
+      .delete<MenuItem>(`${this.baseUrl}/delete-item/${menuItem}`)
+      .pipe(
+        tap(_ => this.log(`deleted item id=${menuItem}`)),
+        catchError(this.handleError("delete-item", menuItem))
+      );
+  }
+
+  /** PUT: update the menu on the server */
+  updateMenuItem(menuItem: MenuItem): Observable<any> {
+    return this.http
+      .put(`${this.baseUrl}/update-item`, menuItem, httpOptions)
+      .pipe(
+        tap(_ => this.log(`updated item id=${menuItem.id}`)),
+        catchError(this.handleError<any>("update-item"))
+      );
+  }
+
+  /* Helper Methods */
+
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+
+  load(): Promise<MenuItem[]> {
+    this.menuItem = new Array<MenuItem>();
+    return this.http
+      .get<MenuItem[]>(`${this.baseUrl}/get-all-items`, httpOptions)
+      .toPromise()
+      .then((data: any) => (this.menuItem = data))
+      .catch((err: any) => Promise.resolve());
+  }
+
+  /** Log a MenuItemService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`MenuItemService: ${message}`);
+  }
+}
--- angular-app/src/assets/Admin/ts/menu-item-add.component.ts
+++ angular-app/src/assets/Admin/ts/menu-item-add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { MenuItemService } from '../../../app/core/services/menu-item.service';
+import { MenuService } from '../../../app/core/services/menu.service';
+import { MenuItem } from '../../../app/models/MenuItem';
+import { Menu } from '../../../app/models/Menu';
 import * as $ from "jquery";
 
 @Component({
@@ -10,11 +15,56 @@ import * as $ from "jquery";
 })
 export class MenuItemAddComponent implements OnInit {
   title = "MENU ITEM"
+  menuItem: MenuItem = new MenuItem();
 
-  constructor() { }
+  menuItemForm : FormGroup;
+  name:  FormControl = new FormControl();
+  description: FormControl = new FormControl();
+  price: FormControl = new FormControl();
+  salePrice: FormControl = new FormControl();
+  cookingTime: FormControl = new FormControl();
+  maxServings: FormControl = new FormControl();
+  imageLink: FormControl = new FormControl();
+  show: FormControl = new FormControl();
+  menuId: FormControl = new FormControl();
+
+  menuList: Array<Menu> = new Array<Menu>();
+
+  constructor(private menuItemService: MenuItemService, private menuService: MenuService) { }
 
   ngOnInit() {
-    
+    //this.getMenus();
+    this.menuItemForm = new FormGroup({
+      name: this.name,
+      description: this.description,
+      price: this.price,
+      salePrice: this.salePrice,
+      cookingTime: this.cookingTime,
+      maxServings: this.maxServings,
+      imageLink: this.imageLink,
+      show: this.show,
+      menuId: this.menuId
+    });
+  }
+
+  addItem() {
+    this.menuItemService.addMenuItem(this.menuItem).subscribe(
+      data => {
+      
+      }
+    );
+  }
+
+  getMenus() {
+    this.menuService.getMenus().subscribe(
+      data => {
+        this.menuList = data;
+      }
+    );
+  }
+
+  onSubmit() {
+    this.addItem();
   }
 
 }
