ff164c323fa552ed23101095bd53e9a2f779afb1
Patrick <papelias1337@gmail.com>
2018-10-07 12:37:09 +0800

    [Task/#0055] - Add Menu Item Functionality

--- angular-app/src/assets/Admin/ts/menu-item-add.component.ts
+++ angular-app/src/assets/Admin/ts/menu-item-add.component.ts
@@ -20,17 +20,19 @@ export class MenuItemAddComponent implements OnInit {
   menuItemForm : FormGroup;
   name:  FormControl = new FormControl('', Validators.required);
   description: FormControl = new FormControl('', Validators.required);
-  price: FormControl = new FormControl();
-  salePrice: FormControl = new FormControl();
-  cookingTime: FormControl = new FormControl();
-  maxServings: FormControl = new FormControl();
+  price: FormControl = new FormControl('', Validators.required);
+  salePrice: FormControl = new FormControl('', Validators.required);
+  cookingTime: FormControl = new FormControl('', Validators.required);
+  maxServings: FormControl = new FormControl('', Validators.required);
   imageLink: FormControl = new FormControl('', Validators.required);
   show: FormControl = new FormControl();
   menuId: FormControl = new FormControl('', Validators.required);
 
   menuList: Array<Menu> = new Array<Menu>();
 
-  constructor(private menuItemService: MenuItemService, private menuService: MenuService) { }
+  constructor(private menuItemService: MenuItemService,
+              private menuService: MenuService,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     //this.getMenus();
@@ -47,10 +49,42 @@ export class MenuItemAddComponent implements OnInit {
     });
   }
 
+
+
   addItem() {
+
+    eval(
+      'swal({' +
+      'title: "Processing",' +
+      'text: "Please wait as we process your request",' +
+      'showConfirmButton: false,' +
+      'confirmButtonColor: "#FBA62F"' +
+      '});'
+    );
+    
     this.menuItemService.addMenuItem(this.menuItem).subscribe(
       data => {
-      
+        if(data != null) {
+          eval(
+            'swal({' +
+            'title: "Success",' +
+            'text: "Successfully added the menu!",' +
+            'type: "success",' +
+            'confirmButtonText: "Okay",' +
+            'confirmButtonColor: "#FBA62F"' +
+            '});'
+          );
+        } else {
+          eval(
+            'swal({' +
+            'title: "Ooops!",' +
+            'text: "There was an error during the process. Please try again!",' +
+            'type: "error",' +
+            'confirmButtonText: "Try Again",' +
+            'confirmButtonColor: "#A40020"' +
+            '});'
+          );
+        }
       }
     );
   }
