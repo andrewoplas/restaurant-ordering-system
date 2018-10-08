cea700323c2cf1b525fa155fb0b49c4d5c5176d9
Andrew Oplas <andrewmoplas@gmail.com>
2018-10-08 12:05:06 +0800

    [Task/#0055] - Done display menu items functionality

--- angular-app/src/assets/Admin/ts/menu-item.component.ts
+++ angular-app/src/assets/Admin/ts/menu-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import { MenuItemService } from '@services/menu-item.service';
+import { MenuItem } from '@models/MenuItem';
 
 @Component({
   selector: 'display-item-menu',
@@ -7,28 +9,43 @@ import * as $ from 'jquery';
   styleUrls: ['../scss/menu-item.component.scss']
 })
 export class MenuItemComponent implements OnInit {
-
-  title = "MAIN COURSE";
+  title = "MENU ITEM";
+  menuItemList: Array<MenuItem> = new Array<MenuItem>();
   
-  constructor() { }
+  constructor(
+    private menuItemService: MenuItemService
+  ) { }
 
 
   ngOnInit() {
-    $('input[type=file]').change(function () {
-      var filePath = $(this).val().replace(/^.*[\\\/]/, '');
-      $("#file-name").text(filePath); 
-    });
+    // $('input[type=file]').change(function () {
+    //   var filePath = $(this).val().replace(/^.*[\\\/]/, '');
+    //   $("#file-name").text(filePath); 
+    // });
+
+    this.menuItemService.getAllMenuItems().subscribe(
+      data => { this.menuItemList = data; }
+    );
   }
 
-  ngAfterViewInit() {
-    $("#table-menu-item").dataTable({
-      columnDefs: [
-        {
-          targets: [0, 5],
-          orderable: false
-        }
-      ]
-    });
+  reinitialize(isLast: boolean) {
+    if (isLast && !$.fn.DataTable.isDataTable('#table-menu-item')) {
+      $('#table-menu-item').dataTable();
+      eval("$('[data-toggle=tooltip]').tooltip();");
+    }
   }
+
+  // delete(id: number) {
+  //   this.menuService.deleteMenu(id)
+  //     .subscribe(
+  //       success => {
+  //         if(success) {
+  //           this.menuList = this.menuList.filter(o => o.id != id);
+  //            alert('done');
+  //         } else {
+  //            alert('error');
+  //         }
+  //     });
+  // }
 
 }
