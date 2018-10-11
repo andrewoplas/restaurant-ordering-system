069e408c1f496900fb4dcf8a34d6735c134d3f1b
Maharlika Rabago <maharlika.rabago@gmail.com>
2018-10-11 17:32:37 +0800

    [Task/#0018] - Added background

---
+++ angular-app/src/assets/ts/dish-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-details',
+  templateUrl: '../html/dish-details.component.html',
+  styleUrls: ['../scss/dish-details.component.scss']
+})
+export class DishDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
